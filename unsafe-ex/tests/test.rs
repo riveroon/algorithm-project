18f3d87937d8de85f0a04bf87fdef2c0b519d2d5
riveroon <73632171+riveroon@users.noreply.github.com>
2024-12-22 17:44:38 +0900

    use allocated objects for all tests

--- unsafe-ex/tests/test.rs
+++ unsafe-ex/tests/test.rs
@@ -1,51 +1,83 @@
+use std::{borrow::Borrow, ops::{Deref, DerefMut}};
+
 pub use unsafe_ex::HashMap;
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
-struct Key(pub usize);
+struct Token(Box<usize>);
+
+impl Token {
+    pub fn new(value: usize) -> Self {
+        Self(Box::new(value))
+    }
+}
+
+impl Borrow<usize> for Token {
+    fn borrow(&self) -> &usize {
+        &self
+    }
+}
+
+impl Deref for Token {
+    type Target = usize;
+
+    fn deref(&self) -> &Self::Target {
+        &*self.0
+    }
+}
+
+impl DerefMut for Token {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut *self.0
+    }
+}
 
 #[test]
 pub fn insert_lookup() {
-    let mut map: HashMap<Key, usize> = HashMap::new();
+    let mut map: HashMap<Token, Token> = HashMap::new();
 
     for i in 0..100 {
-        map.insert(Key(i), !i);
+        map.insert(Token::new(i), Token::new(!i));
     }
 
     assert_eq!(map.len(), 100);
 
     for i in 0..100 {
-        assert_eq!(map.get(&Key(i)), Some(&!i));
+        assert_eq!(map.get(&i), Some(&Token::new(!i)));
     }
 }
 
 #[test]
 pub fn insert_replace() {
-    let mut map: HashMap<Key, usize> = HashMap::new();
+    let mut map: HashMap<Token, Token> = HashMap::new();
 
     for i in 0..100 {
-        map.insert(Key(i), !i);
+        map.insert(Token::new(i), Token::new(!i));
     }
 
     assert_eq!(map.len(), 100);
 
     for i in 0..100 {
-        map.insert(Key(i), i);
+        map.insert(Token::new(i), Token::new(i));
+    }
+
+    assert_eq!(map.len(), 100);
+
+    for i in 0..100 {
+        assert_eq!(map.get(&i), Some(&Token::new(i)));
     }
 }
 
 #[test]
 pub fn remove() {
-    let mut map: HashMap<Key, usize> = HashMap::new();
+    let mut map: HashMap<Token, Token> = HashMap::new();
 
     for i in 0..100 {
-        map.insert(Key(i), !i);
+        map.insert(Token::new(i), Token::new(!i));
     }
 
     for i in 0..100 {
-        let key = Key(i);
-
-        assert_eq!(map.remove(&key), Some(!i));
-        assert_eq!(map.get(&key), None);
+        assert_eq!(map.remove(&i), Some(Token::new(!i)));
+        assert_eq!(map.get(&i), None);
         assert_eq!(map.len(), 99 - i);
     }
 
@@ -54,27 +86,27 @@ pub fn remove() {
 
 #[test]
 pub fn modify_lookup() {
-    let mut map: HashMap<Key, usize> = HashMap::new();
+    let mut map: HashMap<Token, Token> = HashMap::new();
 
     for i in 0..100 {
-        map.insert(Key(i), !i);
+        map.insert(Token::new(i), Token::new(!i));
     }
 
     for i in 0..100 {
-        *map.get_mut(&Key(i)).unwrap() ^= 0b1;
+        **map.get_mut(&i).unwrap() ^= 0b1;
     }
 
     for i in 0..100 {
-        assert_eq!(map.get(&Key(i)), Some(&(!i ^ 0b1)));
+        assert_eq!(map.get(&i), Some(&Token::new(!i ^ 0b1)));
     }
 }
 
 #[test]
 pub fn clear() {
-    let mut map: HashMap<Key, usize> = HashMap::new();
+    let mut map: HashMap<Token, Token> = HashMap::new();
 
     for i in 0..100 {
-        map.insert(Key(i), !i);
+        map.insert(Token::new(i), Token::new(!i));
     }
 
     assert_eq!(map.len(), 100);
@@ -84,24 +116,23 @@ pub fn clear() {
     assert_eq!(map.len(), 0);
 
     for i in 0..100 {
-        assert_eq!(map.get(&Key(i)), None);
+        assert_eq!(map.get(&i), None);
     }
 }
 
 pub fn drain() {
-    let mut map: HashMap<Key, usize> = HashMap::new();
+    let mut map: HashMap<Token, Token> = HashMap::new();
 
     for i in 0..100 {
-        map.insert(Key(i), i);
+        map.insert(Token::new(i), Token::new(i));
     }
 
     let mut returned = [false; 100];
 
-    for (Key(k), v) in map.drain() {
-        assert_eq!(returned[k], false);
-        returned[k] = true;
-
-        assert_eq!(v, k);
+    for (k, v) in map.drain() {
+        assert_eq!(returned[*k], false);
+        returned[*k] = true;
+        assert_eq!(v, k)
     }
 
     assert_eq!(returned, [true; 100]);
