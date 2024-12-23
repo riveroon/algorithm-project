33ac95965f2d6870eec2e652c31c87ab7303fd65
riveroon <73632171+riveroon@users.noreply.github.com>
2024-12-23 23:10:45 +0900

    add safe-iex to bench

--- bench/benches/bench.rs
+++ bench/benches/bench.rs
@@ -1,5 +1,6 @@
 bench::bench!(
     "safe_ex::HashMap" safe_ex::HashMap<_, _, ahash::RandomState>,
+    "safe_iex::HashMap" safe_iex::HashMap<_, _, ahash::RandomState>,
     "unsafe_ex::HashMap" unsafe_ex::HashMap<_, _, ahash::RandomState>,
     "std::collections::HashMap" ahash::AHashMap<_, _>
 );
